83eda0f8479cd582890607004e0ed2403e188a0d
arawayrj <rjohnway@gmail.com>
2017-09-16 22:48:12 -0400

    Added new forms

---
+++ CNC Helper 2/WorkForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CNC_Helper_2
+{
+    public partial class WorkForm : Form
+    {
+        public WorkForm()
+        {
+            InitializeComponent();
+        }
+
+        public WorkForm(string fileName)
+        {
+            InitializeComponent();
+        }
+    }
+}
