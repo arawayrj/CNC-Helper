724a4df6496b9f738599a63a350f7c695ceb7972
RJ Araway <RJ Araway>
2017-09-16 22:21:32 -0400

    Add project files.

---
+++ CNC Helper 2/Classes/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNC_Helper_2.Classes
+{
+    [Serializable]
+    public class Point
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
+}
